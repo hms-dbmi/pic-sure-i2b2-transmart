3e4565c3c8d5b51dd186e3c4e2f14e6ee60dccb9
Gabor Korodi <gabor_korodi@hms.harvard.edu>
2019-02-01 17:16:33 -0500

    Pushing everything

--- httpd/utilities/index.sh
+++ httpd/utilities/index.sh
@@ -1,14 +1,22 @@
 #!/bin/bash -x
 
-docker system info > info.txt
-export DOCKER_MACHINE_NAME=`egrep -e "^Name: " info.txt | cut -d" " -f 2`
+TMPDIR=/tmp
+CWD=`pwd`
+
+source .env
+
+docker system info > ${TMPDIR}/info.txt
+export DOCKER_MACHINE_NAME=`egrep -e "^Name: " ${TMPDIR}/info.txt | cut -d" " -f 2`
 export DOCKER_VERSION=`docker --version`
 export DOCKER_SERVICES_LIST=`docker ps --format '<tr><td>'{{.Names}}'</td><td>'{{.Image}}'</td></tr>' | sort`
-
+export STACK_NAME=$STACK_NAME # Don't ask. Stupid. But we need it!
 CONTAINER_NAME=`docker ps --format {{.Names}} | grep httpd`
-docker exec ${CONTAINER_NAME} printenv > index.txt
+docker exec ${CONTAINER_NAME} printenv > ${TMPDIR}/index.txt
 
-python index.py > index.html
+python \
+  ${CWD}/httpd/utilities/index.py \
+  local ${CWD}/config/httpd/htdocs \
+  /index.html > ${TMPDIR}/index.html
 
-docker cp index.html ${CONTAINER_NAME}:/usr/local/apache2/htdocs/index.html
-rm -f index.html index.txt info.txt
+docker cp ${TMPDIR}/index.html ${CONTAINER_NAME}:/usr/local/apache2/htdocs/index.html
+rm -f ${TMPDIR}/index.html ${TMPDIR}/index.txt ${TMPDIR}/info.txt
