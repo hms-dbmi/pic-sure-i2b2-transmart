6e58631e08b79c4c982168ccb0aa949ee1c0169f
Gabor Korodi <gkorodi@gmail.com>
2019-08-23 14:05:45 -0400

    More configuration script changes

--- config/tools/db_populate.sh
+++ config/tools/db_populate.sh
@@ -1,9 +1,47 @@
 #!/bin/sh
 
-pip3 install pymysql >/dev/null 2>&1
 pip3 install PyJWT >/dev/null 2>&1
-pip3 install requests >/dev/null 2>&1
 
 export PSAMA_CLIENT_SECRET=`grep "java:global/client_secret" /var/tmp/config/wildfly/standalone.xml | cut -d '"' -f 4`
+cat <<EOT >/tmp/get_token.py
+import base64
+import time
+import jwt
 
-python3 /var/tmp/tools/db_populate.py
+signature = base64.b64decode(auth0_secret.replace("_","/").replace("-","+"))
+token = jwt.encode({
+    'email': "configurator@avillach.lab",
+    'sub': "configurator@avillach.lab",
+    "exp": int(time.time())+(expire_time*60*60),
+    "iat": int(time.time())
+    },
+    '${PSAMA_CLIENT_SECRET}',
+    algorithm='HS256'
+)
+print(token.decode('utf-8'))
+EOT
+export AUTOMATA_USER_TOKEN=$(python /tmp/get_token.py)
+
+export HOSTNAME="https://host.docker.internal/psama"
+
+addApplication() {
+	URL="${HOSTNAME}/psama/application"
+	
+	APP_NAME=$1
+	APP_DESCRIPTION=$2
+	APP_URL=$3
+	
+	echo '[{"uuid":"","name":"'$APP_NAME'","description":"'$APP_DESCRIPTION'","url":"'$APP_URL'"}]' > /tmp/req_application.json
+	
+	curl -k -X POST \
+		-H "Authorization: Bearer ${AUTOMATA_USER_TOKEN}" \
+		-H "Content-type: application/json" \
+		--data @/tmp/req_application.json \
+		--output /tmp/resp_application.json \
+		$URL
+
+}
+
+addApplication 'TRANSMART' 'i2b2/tranSmart Web Application' '/transmart'
+addApplication 'PICSURE' 'PIC-SURE multiple data access API' '/picsureui'
+addApplication 'IRCT' 'IRCT data access API'
